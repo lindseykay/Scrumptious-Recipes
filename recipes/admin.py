8c4bb89646acca2146a11158f34bfdc7df3cc464
Lindsey <lindseykaycarlson@outlook.com>
2022-08-24 15:53:47 -0700

    add the Recipe Model

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 
 
 # Register your models here.
+from recipes.models import Recipe
+
+admin.site.register(Recipe)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,23 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + "by" + self.author
+
+
+# if you have nothing in your class/model,
+# Django automatically gives the instance an ID integer
+
+
+# when you update models:
+
+# 1. update the model/ create the model
+# 2. make migrations
