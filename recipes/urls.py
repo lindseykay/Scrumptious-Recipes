152d4bcfdca516f3203f1d60dc4319c4d8c083cb
Lindsey <lindseykaycarlson@outlook.com>
2022-08-29 16:54:10 -0700

    Converted detail function to detail class

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,14 +3,15 @@ from django.urls import path
 from recipes.views import (
     create_recipe,
     change_recipe,
-    show_recipe,
-    #show_recipes,
-    RecipeListView
+    # show_recipe,
+    # show_recipes,
+    RecipeListView,
+    RecipeDetailView,
 )
 
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
-    path("<int:pk>/", show_recipe, name="recipe_detail"),
+    path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("new/", create_recipe, name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
 ]
