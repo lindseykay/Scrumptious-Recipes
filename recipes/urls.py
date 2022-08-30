0eff4771c4c0318d48543065d8c3041069ca2622
Lindsey <lindseykaycarlson@outlook.com>
2022-08-29 20:16:59 -0700

    Converted detail function to Detail class

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,14 +4,15 @@ from recipes.views import (
     create_recipe,
     change_recipe,
     log_rating,
-    show_recipe,
-    RecipeListView,
+    # show_recipe,
     # show_recipes,
+    RecipeListView,
+    RecipeDetailView,
 )
 
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
-    path("<int:pk>/", show_recipe, name="recipe_detail"),
+    path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("new/", create_recipe, name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
